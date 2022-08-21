325b3351eeebe0221d602a672c1310915dbd80ce
Michal Radomski <michalra@op.pl>
2022-08-21 23:36:17 +0200

    navigation menu - commit1

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,21 @@
+// Input -> console.log
 const nameInput = document.getElementById("inputName");
 document.querySelector(".form--input").addEventListener("submit", function (event) {
   //prevent the normal submission of the form
   event.preventDefault();
   console.log(nameInput.value);
 });
+
+// Open/ Close menu
+const menu = document.querySelectorAll(".menu--navigation > .link");
+// console.log({menu});
+function openCloseMenu() {
+  menu.forEach((elem) => elem.classList.toggle("responsive"));
+}
+
+// Close menu on menu link click
+menu.forEach((elem) => elem.addEventListener("click", removeResponsive));
+
+function removeResponsive() {
+  menu.forEach((elem) => elem.classList.remove("responsive"));
+}
