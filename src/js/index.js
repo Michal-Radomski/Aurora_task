9ac7ae88283fcf0891a1102a0300fb98d37caff0
Michal Radomski <michalra@op.pl>
2022-08-22 15:21:23 +0200

    Layout done

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,6 @@
 // Input -> console.log
 const nameInput = document.getElementById("inputName");
 document.querySelector(".form--input").addEventListener("submit", function (event) {
-  //prevent the normal submission of the form
   event.preventDefault();
   console.log(nameInput.value);
 });
